a2f075a64bbde83af16158b05ddda965273e62cb
lahceenn <lahcenouhassou18@gmail.com>
2025-11-28 20:32:44 +0100

    UpdTodo...

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   res.send("Server is Running ✅");
 });
 
-const PORT = 5000;
+const PORT = 6000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
